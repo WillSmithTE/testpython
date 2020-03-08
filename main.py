ebc619a85b7348b87f45919686e3da56282fb578
Will <willsmithte@gmail.com>
2020-03-08 17:26:50 +1100

    progres

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import pandas as pd
 import numpy as np
 
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import OneHotEncoder
 
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
 
 train_x = train_df[['name']]
-print(train_x)
 train_y = train_df[['Goat-status']]
-test_x = test_df[['name']]
+test_x = pd.read_csv('test.csv')[['name']]
+
+encoder = OneHotEncoder()
+encoder.fit(train_x)
+train_x_featurized = encoder.transform(train_x)
+test_x_featurized = encoder.transform(test_x)
 
 neigh = KNeighborsClassifier(n_neighbors=1)
-neigh.fit(train_x, train_y)
-prediction = neigh.predict(test_x)
+neigh.fit(train_x_featurized, train_y.values.ravel())
+prediction = neigh.predict(test_x_featurized)
 
-print(prediction)
+if prediction:
+	print('GOAT!')
+else:
+	print('not the goat ...')
