7c0c5376e6cc433e2f8300731ffdae64e01e2d86
Will <willsmithte@gmail.com>
2020-03-08 23:31:25 +1100

    progress

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 import pandas as pd
 import numpy as np
+import pickle
+import json
 
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neighbors import RadiusNeighborsRegressor
 from sklearn.preprocessing import OneHotEncoder
+from flask import Flask, request, redirect, url_for, flash, jsonify
+
+input = 'patty mills'
+
+MODEL_FILE_NAME = 'model.pickle';
 
 train_df = pd.read_csv('train.csv')
-test_df = pd.read_csv('test.csv')
+
+data = { 'name': [input] }
+
+def formatPrediction(prediction):
+	if prediction:
+		return 'GOAT!'
+	else:
+		return 'not the goat ...'
 
 train_x = train_df[['name']]
 train_y = train_df[['Goat-status']]
 test_x = pd.read_csv('test.csv')[['name']]
+test_x = pd.DataFrame(data)
 
 encoder = OneHotEncoder(handle_unknown='ignore')
 encoder.fit(train_x)
 train_x_featurized = encoder.transform(train_x)
 test_x_featurized = encoder.transform(test_x)
 
-neigh = RadiusNeighborsRegressor(radius=1.0)
+neigh = KNeighborsClassifier(n_neighbors=1)
 neigh.fit(train_x_featurized, train_y.values.ravel())
-prediction = neigh.predict(test_x_featurized)
 
-if prediction:
-	print('GOAT!')
-else:
-	print('not the goat ...')
+pickle.dump(neigh, open(MODEL_FILE_NAME, 'wb'))
+
+prediction = neigh.predict(test_x_featurized)
+print(formatPrediction(prediction))
+
+app = Flask(__name__)
+
+@app.route('/goat/<name>', methods=['GET'])
+def getPrediction(name):
+	data = pd.DataFrame({ 'name': [name] })
+	print(data)
+	encoded = encoder.transform(data)
+	prediction = np.array2string(model.predict(encoded))
+	return formatPrediction(prediction)
+
+if __name__ == '__main__':
+	model = pickle.load(open(MODEL_FILE_NAME, 'rb'))
+	app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', '5000'))
