5506696d5d894c0f2f36c627e9e2104b74c87d88
Will Smith <willsmithte@gmail.com>
2020-03-16 20:35:13 +1100

    format, add middleName and draftNumber to model

--- main.py
+++ main.py
@@ -15,31 +15,41 @@ MODEL_FILE_NAME = 'model.pickle'
 
 dataset = pd.read_csv('train.csv')
 
-CATEGORICAL_FEATURES = [ 'college' ]
+CATEGORICAL_FEATURES = ['college']
+
 
 def formatPrediction(prediction):
-	if prediction == '100':
-		return 'G O A T'
-	else:
-		return prediction + ' ? Definitely not a goat ...'
+    if prediction == '100':
+        return 'G O A T'
+    else:
+        return prediction + ' ? Definitely not a goat ...'
+
 
 train_y = dataset[['score']]
 
+
 def transformData(data):
-	output = data
-	for feature in CATEGORICAL_FEATURES:
-		categorical = pd.Categorical(output[feature])
-		dummies = pd.get_dummies(categorical, prefix=feature, columns=categorical.unique())
-		output = pd.concat([ output, dummies ])
-		del output[feature]
-	return output
+    output = data
+    for feature in CATEGORICAL_FEATURES:
+        categorical = pd.Categorical(output[feature])
+        dummies = pd.get_dummies(
+            categorical, prefix=feature, columns=categorical.unique())
+        output = pd.concat([output, dummies])
+        del output[feature]
+    return output
+
 
 del dataset['name']
 del dataset['score']
 dataset = pd.get_dummies(dataset)
 
-
-classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5,2), max_iter=2000, random_state=1)
+classifier = MLPClassifier(
+    solver='lbfgs',
+    alpha=1e-5,
+    hidden_layer_sizes=(5, 2),
+    max_iter=2000,
+    random_state=1
+)
 classifier.fit(dataset, train_y.values.ravel())
 
 # pickle.dump(classifier, open(MODEL_FILE_NAME, 'wb'))
@@ -47,16 +57,26 @@ classifier.fit(dataset, train_y.values.ravel())
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/goat', methods=['GET'])
 def getPrediction():
-	college = request.args.get('college')
-	rings = request.args.get('rings')
-	siblings = request.args.get('siblings')
-	asDataFrame = pd.DataFrame({ 'college_' + college: [1], 'rings': [rings], 'siblings': [siblings] })
-	asDataFrame = asDataFrame.reindex(columns = dataset.columns, fill_value=0)
-	prediction = np.array2string(classifier.predict(asDataFrame)[0])
-	return json.dumps(formatPrediction(prediction))
+    college = request.args.get('college')
+    rings = request.args.get('rings')
+    siblings = request.args.get('siblings')
+    middleNameCount = request.args.get('middleNameCount')
+    draftNumber = request.args.get('draftNumber')
+    asDataFrame = pd.DataFrame({
+        'college_' + college: [1],
+        'rings': [rings],
+        'siblings': [siblings],
+        'middleNameCount': [middleNameCount],
+        'draftNumber': [draftNumber]
+    })
+    asDataFrame = asDataFrame.reindex(columns=dataset.columns, fill_value=0)
+    prediction = np.array2string(classifier.predict(asDataFrame)[0])
+    return json.dumps(formatPrediction(prediction))
+
 
 if __name__ == '__main__':
-	# model = pickle.load(open(MODEL_FILE_NAME, 'rb'))
-	app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', '5000'))
+    # model = pickle.load(open(MODEL_FILE_NAME, 'rb'))
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', '5000'))
